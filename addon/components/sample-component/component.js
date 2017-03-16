08dee73609f3ae2c9ad8056aa6d42680d55604c3
Sam Selikoff <sam.selikoff@gmail.com>
2017-03-16 02:38:37 -0400

    Add a component with YUI docs

---
+++ addon/components/sample-component/component.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import layout from './template';
+
+export default Ember.Component.extend({
+  
+  layout,
+
+  /**
+    The best method ever made, anywhere.
+    @method impressUser
+    @param {Object} features the features of the user
+    @param {String} name the name of the user
+    @return {String} the welcome message
+    @public
+  */
+  impressUser(features, name) {
+    return `${name} has features ${features}`;
+  }
+
+});
