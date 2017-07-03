9920444eeb0ead20925ca7eea67ca5090d3cc8f1
Dan Freeman <dfreeman@salsify.com>
2017-07-03 19:05:02 -0400

    Perhaps get docs fetching correctly on gh-pages

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,18 +1,20 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import config from 'dummy/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
 
-  host: '/docs',
+  namespace: `${config.rootURL.replace(/\/$/, '')}/docs`,
 
   projectVersion: Ember.inject.service(),
 
   buildURL(modelName, id, snapshot, requestType, query) {
     let url = this._super(...arguments);
+    let namespace = this.get('namespace');
 
     if (modelName !== 'project') {
       let version = this.get('projectVersion');
-      url = `/docs/${version.version}/${url.substr(5)}`;
+      url = `${namespace}/${version.version}/${url.substr(namespace.length + 1)}`;
     }
 
     return `${url}.json`;
