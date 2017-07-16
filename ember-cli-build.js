950ae19dd9d4f6420c103b80a3468fb6c4e71114
Sam Selikoff <sam.selikoff@gmail.com>
2017-07-16 15:32:51 -0400

    Asset fingerprint config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    fingerprint: {
+      prepend: '/ember-cli-addon-docs/',
+    },
     snippetSearchPaths: ['tests/dummy/app'],
     svgJar: {
       sourceDirs: ['tests/dummy/public']
