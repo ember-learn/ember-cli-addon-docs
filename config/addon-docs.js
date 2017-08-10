d8caa6d60c9fc451f473791a85dba3020841f35c
Dan Freeman <dfreeman@salsify.com>
2017-08-10 15:36:21 -0400

    Use git-repo-info

--- config/addon-docs.js
+++ config/addon-docs.js
@@ -2,10 +2,10 @@
 'use strict';
 
 module.exports = {
-  shouldDeploy(branch, tag) {
+  shouldDeploy(repoInfo) {
     if (process.env.CI) {
       // If in CI, deploy whenever we do a build on master in the default ember-try scenario
-      return branch === 'master' && process.env.EMBER_TRY_SCENARIO === 'ember-default';
+      return repoInfo.branch === 'master' && process.env.EMBER_TRY_SCENARIO === 'ember-default';
     } else {
       // Always deploy when `ember deploy` is manually run
       return true;
