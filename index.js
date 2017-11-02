cbf22d56612741c6966c1aee55b1b865eaf7397b
Dan Freeman <dfreeman@salsify.com>
2017-11-02 17:11:42 -0400

    Make ESLint happy

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ module.exports = {
 
     includer.options.snippetSearchPaths = includer.options.snippetSearchPaths || ['tests/dummy/app'];
     includer.options.snippetRegexes = Object.assign({}, {
-      begin: /{{#(?:docs-snippet|demo.example|demo.live-example)\sname=(?:\"|\')(\S+)(?:\"|\')/,
+      begin: /{{#(?:docs-snippet|demo.example|demo.live-example)\sname=(?:"|')(\S+)(?:"|')/,
       end: /{{\/(?:docs-snippet|demo.example|demo.live-example)}}/,
     }, includer.options.snippetRegexes);
 
