dc51d7cbd18bb173ff7818f09d1cb86aa9f6d0dc
Emmanuel Bourgerie <manu@bourgerie.fr>
2018-02-05 15:59:26 +0000

    Add id to example component

--- addon/components/docs-demo/x-example/component.js
+++ addon/components/docs-demo/x-example/component.js
@@ -4,5 +4,10 @@ import layout from './template';
 export default Component.extend({
   layout,
 
-  classNames: 'docs-demo-example'
+  classNames: 'docs-demo-example',
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('elementId', 'example-' + this.get('name'));
+  }
 });
