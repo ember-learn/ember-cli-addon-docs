eb4df06ebb7709c8aef6b0c95b94dd3dc6862e9a
Dan Freeman <dfreeman@salsify.com>
2018-03-01 23:52:41 -0500

    Fix deploys with new Travis setup

--- config/addon-docs.js
+++ config/addon-docs.js
@@ -4,14 +4,5 @@
 const AddonDocsConfig = require('../lib/config');
 
 module.exports = class extends AddonDocsConfig {
-  shouldDeploy() {
-    let env = process.env;
-    if (env.CI) {
-      // If in CI, deploy whenever we do a build on master in the default ember-try scenario
-      return env.TRAVIS_BRANCH === 'master' && env.EMBER_TRY_SCENARIO === 'ember-default';
-    } else {
-      // Always deploy when `ember deploy` is manually run
-      return true;
-    }
-  }
+
 }
