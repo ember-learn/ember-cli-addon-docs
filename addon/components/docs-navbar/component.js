e1fe2eaebe78dccb7c610fe3471ce6b1267e20f8
Sam Selikoff <sam.selikoff@gmail.com>
2018-03-16 19:41:22 -0700

    More touchups

--- addon/components/docs-navbar/component.js
+++ addon/components/docs-navbar/component.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 import layout from './template';
 import config from 'dummy/config/environment';
+import { computed } from '@ember/object';
+import { classify } from '@ember/string';
 
 const packageJson = config['ember-cli-addon-docs'].packageJson;
 
@@ -16,6 +18,30 @@ export default Component.extend({
 
   tagName: '',
 
-  githubUrl: packageJson.repository
+  packageJson: packageJson,
+
+  addonLogo: computed(function() {
+    let name = packageJson.name;
+    let logo;
+
+    if (name.match('ember-cli-')) {
+      logo = 'ember-cli';
+    } else if (name.match('ember-data-')) {
+      logo = 'ember-data';
+    } else if (name.match('ember-data-')) {
+      logo = 'ember';
+    }
+
+    return logo;
+  }),
+
+  addonName: computed(function() {
+    let name = packageJson.name;
+    name = name.replace('ember-data-', '');
+    name = name.replace('ember-cli-', '');
+    name = name.replace('ember-', '');
+
+    return classify(name);
+  })
 
 });
