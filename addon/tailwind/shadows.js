fda184b17122071e44a3eeba579630509cc12708
Sam Selikoff <sam.selikoff@gmail.com>
2018-03-24 12:35:46 -0400

    Powered by addon docs treatment

--- addon/tailwind/shadows.js
+++ addon/tailwind/shadows.js
@@ -15,6 +15,7 @@
 */
 
 export default {
+  'sm': '0 1px 1px 0 rgba(116, 129, 141, 0.1)',
   default: '0 2px 4px 0 rgba(0,0,0,0.10)',
   'md': '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
   'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
