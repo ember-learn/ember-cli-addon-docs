6bbb803678e7cb7d0b53c7cb4313e572df7084ae
Sam Selikoff <sam.selikoff@gmail.com>
2018-03-25 01:56:43 -0400

    Mobile header

---
+++ addon/initializers/inject-media.js
@@ -0,0 +1,8 @@
+export function initialize(application) {
+  application.inject('component', 'media', 'service:media');
+  application.inject('controller', 'media', 'service:media');
+}
+
+export default {
+  initialize
+};
