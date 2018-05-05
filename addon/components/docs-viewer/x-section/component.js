0d48efb57b89785239a1ccb733662b9fd9b49724
Christopher Garrett <opensource@pzuraq.com>
2018-05-04 18:33:24 -0700

    [cleanup] Simplify the API doc navigation
    * Simplifies the API doc navigation. It now only shows the modules
    themselves, and none of their items (that is left to the API docs page).
    
    * Brings back nested subnavigation menus
    
    * Adds the `break-on` helper which allows us to specify when a string
    should break by inserting zero-width spaces

--- addon/components/docs-viewer/x-section/component.js
+++ addon/components/docs-viewer/x-section/component.js
@@ -1,12 +1,18 @@
 import Component from '@ember/component';
-import layout from './template';
+import hbs from 'htmlbars-inline-precompile';
 
 export default Component.extend({
+  tagName: '',
 
-  layout,
-
-  tagName: ''
-
+  layout: hbs`
+    <li class='
+      mt-8 text-grey font-bold
+      tracking-wide uppercase
+      {{if (eq style 'large') '-mb-4 text-sm' 'text-xs'}}
+    '>
+      {{label}}
+    </li>
+  `
 }).reopenClass({
 
   positionalParams: [ 'label' ]
