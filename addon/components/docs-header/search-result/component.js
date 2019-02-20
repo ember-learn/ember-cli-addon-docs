0fa77ca907965360b2d029661096830695e33ae0
Sam Selikoff <sam.selikoff@gmail.com>
2019-02-20 16:53:54 -0500

    Slightly better search ui... (#324)

--- addon/components/docs-header/search-result/component.js
+++ addon/components/docs-header/search-result/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import layout from './template';
 import { computed } from '@ember/object';
+import { bool } from '@ember/object/computed';
 
 export default Component.extend({
   layout,
@@ -40,48 +41,66 @@ export default Component.extend({
   matches: computed(function() {
     let metadata = this.get('result.resultInfo.matchData.metadata');
 
-    return Object.keys(metadata).reduce((matches, term) => {
-      let match = metadata[term];
-      let query = this.get('query');
-      let normalizedQuery = query.toLowerCase();
-      Object.keys(match).forEach((key) => {
-        if (key === 'text') {
-          let text = this.get('result.document.text');
-          let spaceIndices = text.split("")
-            .map((char, index) => (char === ' ') ? index : null)
-            .filter(val => val > 0);
+    return Object.keys(metadata)
+      .reduce((matches, term) => {
+        let match = metadata[term];
+        let query = this.get('query');
+        let normalizedQuery = query.toLowerCase();
+        Object.keys(match).forEach((key) => {
+          if (key === 'text') {
+            let text = this.get('result.document.text');
+            let spaceIndices = text.split("")
+              .map((char, index) => (char === ' ') ? index : null)
+              .filter(val => val > 0);
 
-          match.text.position.forEach(([ wordStart, length ]) => {
-            let spaceAfterWord = spaceIndices.find(i => i > wordStart);
-            let indexOfSpaceAfterWord = spaceIndices.indexOf(spaceAfterWord);
-            let indexOfSpaceBeforeWord = indexOfSpaceAfterWord - 1;
-            let indexOfStartingSpace = (indexOfSpaceBeforeWord > 3) ? indexOfSpaceBeforeWord - 3 : 0;
-            let indexOfEndingSpace = ((indexOfSpaceAfterWord + 3) < spaceIndices.length) ? indexOfSpaceAfterWord + 3 : spaceIndices.length;
-            let matchingText = text.slice(spaceIndices[indexOfStartingSpace], spaceIndices[indexOfEndingSpace]);
-            matchingText = this._highlight(matchingText, matchingText.indexOf(query), query.length);
+            match.text.position.forEach(([ wordStart, length ]) => {
+              let spaceAfterWord = spaceIndices.find(i => i > wordStart);
+              let indexOfSpaceAfterWord = spaceIndices.indexOf(spaceAfterWord);
+              let indexOfSpaceBeforeWord = indexOfSpaceAfterWord - 1;
+              let indexOfStartingSpace = (indexOfSpaceBeforeWord > 3) ? indexOfSpaceBeforeWord - 3 : 0;
+              let indexOfEndingSpace = ((indexOfSpaceAfterWord + 3) < spaceIndices.length) ? indexOfSpaceAfterWord + 3 : spaceIndices.length;
+              let matchingText = text.slice(spaceIndices[indexOfStartingSpace], spaceIndices[indexOfEndingSpace]);
+              matchingText = this._highlight(matchingText, matchingText.indexOf(query), query.length);
 
-            matches.push(matchingText);
-          });
-        } else {
-          let normalizedTerm = term.toLowerCase();
-          this.get('result.document.keywords').forEach((keyword) => {
-            let normalizedKeyword = keyword.toLowerCase();
-            if (keyword.toLowerCase().indexOf(normalizedTerm) !== -1) {
-              let index = normalizedKeyword.indexOf(normalizedQuery);
-              matches.push(this._highlight(keyword, index, normalizedQuery.length));
-            }
-          });
-        }
-      });
+              matches.push(matchingText);
+            });
+          } else {
+            let normalizedTerm = term.toLowerCase();
+            this.get('result.document.keywords').forEach((keyword) => {
+              let normalizedKeyword = keyword.toLowerCase();
+              if (keyword.toLowerCase().indexOf(normalizedTerm) !== -1) {
+                let index = normalizedKeyword.indexOf(normalizedQuery);
+                matches.push(this._highlight(keyword, index, normalizedQuery.length));
+              }
+            });
+          }
+        });
 
-      return matches;
-    }, [])
-      .slice(0, 5)
-      .join(' &middot; ');
+        return matches;
+      }, []);
   }),
 
+  bestMatch: computed('matches.[]', function() {
+    // Right now this is arbitrarily returning the first match. Needs more work to find the "best" match on the page.
+    return this.matches[0];
+  }),
+
+  highlightedTitle: computed('result.document.title', 'query', function() {
+    let title = this.result.document.title;
+    let match = title.match(new RegExp(this.query, 'i'));
+
+    if (match) {
+      let start = match.index;
+      let length = this.query.length;
+
+      return `${title.slice(0, start)}<span class='docs-border-b-2 docs-border-brand'>${title.slice(start, start + length)}</span>${title.slice(start + length)}`;
+    }
+  }),
+
+  titleMatchesQuery: bool('highlightedTitle'),
+
   _highlight(text, start, length) {
-    return `${text.slice(0, start)}<em class='docs-bg-yellow'>${text.slice(start, start + length)}</em>${text.slice(start + length)}`;
+    return `${text.slice(0, start)}<span class='docs-border-b-2 docs-border-brand'>${text.slice(start, start + length)}</span>${text.slice(start + length)}`;
   },
 
   'data-test-search-result': true,
