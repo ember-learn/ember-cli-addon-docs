167a3c1f81afc1125317711754142642d1c33a27
Sam Selikoff <sam.selikoff@gmail.com>
2019-03-21 08:57:47 -0700

    feat: Better nav for generic modules
    Closes #353

--- addon/components/api/x-import-path/component.js
+++ addon/components/api/x-import-path/component.js
@@ -4,17 +4,19 @@ import hbs from 'htmlbars-inline-precompile';
 export default Component.extend({
   tagName: '',
   layout: hbs`
-    <div class="import-path docs-mb-6 docs-font-mono" data-test-import-path>
-      <span class="hljs-keyword">import</span>
+    <div class="import-path docs-mb-6" data-test-import-path>
+      <pre class="docs-md__code docs-whitespace-no-wrap">
+        <span class="hljs-keyword">import</span>
 
-      {{#if (eq item.exportType "default")}}
-        {{item.name}}
-      {{else}}
-      { {{item.name}} }
-      {{/if}}
+        {{#if (eq item.exportType "default")}}
+          {{item.name}}
+        {{else}}
+        { {{item.name}} }
+        {{/if}}
 
-      <span class="hljs-keyword">from</span>
-      <span class="hljs-string">'{{item.file}}'</span>;
+        <span class="hljs-keyword">from</span>
+        <span class="hljs-string">'{{item.file}}'</span>;
+      </pre>
     </div>
   `,
 });
--- addon/components/docs-viewer/x-autogenerated-api-docs/module-nav/component.js
+++ addon/components/docs-viewer/x-autogenerated-api-docs/module-nav/component.js
@@ -1,12 +1,10 @@
 import Component from '@ember/component';
 import layout from './template';
 
+/*
+  A component used to recursively render a nested structure of module nodes.
+*/
 export default Component.extend({
   layout,
-  tagName: '',
-  style: 'regular'
-}).reopenClass({
-
-  positionalParams: [ 'label' ]
-
+  tagName: ''
 });
