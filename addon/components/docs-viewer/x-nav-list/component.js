0f76446885a74f58afa5df52300f50c9e3f4ee16
Peter Banka <pbanka@fastly.com>
2019-12-18 15:51:56 -0800

    reinstate list-reset styles

--- addon/components/docs-viewer/x-nav-list/component.js
+++ addon/components/docs-viewer/x-nav-list/component.js
@@ -4,6 +4,8 @@ import { hbs } from 'ember-cli-htmlbars';
 export default Component.extend({
   tagName: 'ul',
 
+  classNames: ['docs-list-none'],
+
   layout: hbs`
     {{yield (hash
       item=(component 'docs-viewer/x-nav-item')
