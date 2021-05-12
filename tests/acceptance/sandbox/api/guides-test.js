3d5cce1c6d8412d74cde6a3d19a621ed58280658
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2021-05-12 17:04:37 -0400

    Fixed deprecations this.property-fallback, with, and positional linkto parms

--- tests/acceptance/sandbox/api/guides-test.js
+++ tests/acceptance/sandbox/api/guides-test.js
@@ -11,7 +11,7 @@ module('Acceptance | Sandbox | API | Guides', function(hooks) {
 
   test('page index works', async function(assert) {
     await visit('/sandbox');
-
+debugger;
     assert.equal(currentURL(), `/sandbox`, 'correct url');
 
     let indexItems = modulePage.index.items.map(i => i.text);
