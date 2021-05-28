cbcf764bf4eafb20f8bb817c58dfb2500d4802c3
Robert Wagner <rwwagner90@gmail.com>
2021-05-28 14:54:22 -0400

    Fix import

--- addon/components/docs-header/search-box/component.js
+++ addon/components/docs-header/search-box/component.js
@@ -4,7 +4,7 @@ import { task } from 'ember-concurrency';
 import config from 'ember-get-config';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { classNames, layout } from '@ember/decorators';
+import { classNames, layout } from '@ember-decorators/component';
 import { formElementHasFocus } from 'ember-cli-addon-docs/keyboard-config';
 
 const projectName = config['ember-cli-addon-docs'].projectName;
