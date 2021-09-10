6cf2b99ce4fc3dd7ca6689af29abb64c11528bec
Robert Wagner <rwwagner90@gmail.com>
2021-09-10 18:09:08 -0400

    Fix some testing issues

--- addon/components/docs-header/search-result/component.js
+++ addon/components/docs-header/search-result/component.js
@@ -21,33 +21,8 @@ export default Component.extend({
       } else {
         args = ['docs.api.item', this.get('result.model.routingId')];
       }
+
       return args;
-    }
-  ),
-
-  model: computed(
-    'result.document.{route,type}',
-    'result.model.routingId',
-    function () {
-      let type = this.get('result.document.type');
-
-      if (type !== 'template') {
-        return this.get('result.model.routingId');
-      }
-
-      return undefined;
-    }
-  ),
-
-  route: computed(
-    'result.document.{route,type}',
-    'result.model.routingId',
-    function () {
-      let type = this.get('result.document.type');
-
-      return type === 'template'
-        ? this.get('result.document.route')
-        : 'docs.api.item';
     }
   ),
 
