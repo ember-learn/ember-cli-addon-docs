e24f311d94b201db679bd671d916b9d38d889e02
Sergey Astapov <SergeAstapov@gmail.com>
2021-12-02 10:11:05 -0500

    Add babel.config.js to .npmignore (#1031)

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,9 @@
-/* eslint-env node */
+// TODO: this is currently only being used by ESLint via @babel/eslint-parser.
+// When https://github.com/babel/ember-cli-babel/issues/418 gets resolved,
+// we can switch to using it throughout properly.
+
+const { buildEmberPlugins } = require('ember-cli-babel');
 
 module.exports = {
-  plugins: [
-    [
-      require('@babel/plugin-proposal-decorators').default,
-      {
-        legacy: true,
-      },
-    ],
-  ],
+  plugins: [...buildEmberPlugins(__dirname)],
 };
