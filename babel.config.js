f67aa224e5228960b7038a231a0ee6a09af4d351
webdev0415 <igor.shevk@gmail.com>
2022-02-09 10:06:52 -0500

    eslint fix

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 // TODO: this is currently only being used by ESLint via @babel/eslint-parser.
 // When https://github.com/babel/ember-cli-babel/issues/418 gets resolved,
-// we can switch to using it throughout properly
+// we can switch to using it throughout properly.
 
 const { buildEmberPlugins } = require('ember-cli-babel');
 
