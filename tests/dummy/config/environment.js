f9890cb0481ff013ed272058df01ddd890e31386
Robert Wagner <rwwagner90@gmail.com>
2022-02-11 11:52:31 -0500

    ember-cli 4.1.1

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,8 +6,7 @@ module.exports = function (environment) {
     podModulePrefix: 'dummy/pods',
     environment,
     rootURL: '/',
-    locationType: 'auto',
-    historySupportMiddleware: true,
+    locationType: 'history',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
