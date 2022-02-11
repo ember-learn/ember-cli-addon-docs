1918b2a36f1f82a60663fc79d2b30f3fecc0764d
Jonas Metzener <jonas.metzener@adfinis.com>
2022-02-11 11:43:35 +0100

    fix(config): use runtime config instead of embroider generated one

--- addon/adapters/-addon-docs.js
+++ addon/adapters/-addon-docs.js
@@ -1,10 +1,15 @@
+import { getOwner } from '@ember/application';
 import Adapter from '@ember-data/adapter';
-import config from 'ember-get-config';
 import fetch from 'fetch';
 
 export default class AddonDocsAdapter extends Adapter {
   defaultSerializer = '-addon-docs';
-  namespace = `${config.rootURL.replace(/\/$/, '')}/docs`;
+
+  get namespace() {
+    const rootURL =
+      getOwner(this).resolveRegistration('config:environment').rootURL;
+    return `${rootURL.replace(/\/$/, '')}/docs`;
+  }
 
   shouldBackgroundReloadAll() {
     return false;
