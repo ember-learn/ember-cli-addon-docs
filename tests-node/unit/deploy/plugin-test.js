6a9eeff7e35327a8d259a472ccf396fa07c4e1af
Robbie Wagner <rwwagner90@gmail.com>
2024-11-20 08:10:01 -0500

    Update plugin-test.js

--- tests-node/unit/deploy/plugin-test.js
+++ tests-node/unit/deploy/plugin-test.js
@@ -6,7 +6,7 @@ const {
 } = require('../../../lib/utils/find-and-replace-in-directory');
 
 describe('`deploy` | plugin test', function () {
-  it('replaceAddonDocsRootURL', function () {
+  it('replaceAddonDocsRootURL in index.html', function () {
     const contents = `
       <!DOCTYPE html>
       <html>
@@ -23,14 +23,14 @@ describe('`deploy` | plugin test', function () {
     `;
     const encodedVersion = encodeURIComponent(
       JSON.stringify({
-        path: 'versions/master',
-        name: 'master',
+        path: 'versions/main',
+        name: 'main',
         sha: 'eef3',
         tag: null,
-        key: 'master',
+        key: 'main',
       }),
     );
-    const addonDocsRootURL = '/my-addon/versions/master/';
+    const addonDocsRootURL = '/my-addon/versions/main/';
     const actual = replaceAddonDocsRootURL(
       contents,
       addonDocsRootURL,
@@ -40,17 +40,39 @@ describe('`deploy` | plugin test', function () {
       <!DOCTYPE html>
       <html>
         <head>
-          <meta name="dummy/config/environment" content="%7B%22rootURL%22%3A%22%2Fmy-addon%2Fversions%2Fmaster%2F%22%7D" />
+          <meta name="dummy/config/environment" content="%7B%22rootURL%22%3A%22%2Fmy-addon%2Fversions%2Fmain%2F%22%7D" />
         </head>
         <body>
-          <script src="/my-addon/versions/master/assets/chunk.805.d0514e7e9edb751c6623.js"></script>
-          <script src="/my-addon/versions/master/assets/chunk.524.385868f2db0b958b5ced.js"></script>
-          <script src="/my-addon/versions/master/assets/vendor.js"></script>
-          <script src="/my-addon/versions/master/assets/dummy.js"></script>
+          <script src="/my-addon/versions/main/assets/chunk.805.d0514e7e9edb751c6623.js"></script>
+          <script src="/my-addon/versions/main/assets/chunk.524.385868f2db0b958b5ced.js"></script>
+          <script src="/my-addon/versions/main/assets/vendor.js"></script>
+          <script src="/my-addon/versions/main/assets/dummy.js"></script>
         </body>
       </html>
     `;
 
+    assert.equal(actual, expected);
+  });
+  it('replaceAddonDocsRootURL in chunks', function () {
+    const chunk =
+      '(e.children=[]),e),o.p="ADDON_DOCS_ROOT_URL/assets/",(()=>{var e={143:0}';
+    const encodedVersion = encodeURIComponent(
+      JSON.stringify({
+        path: 'versions/main',
+        name: 'main',
+        sha: 'eef3',
+        tag: null,
+        key: 'main',
+      }),
+    );
+    const addonDocsRootURL = '/my-addon/versions/main/';
+    const actual = replaceAddonDocsRootURL(
+      chunk,
+      addonDocsRootURL,
+      encodedVersion,
+    );
+    const expected =
+      '(e.children=[]),e),o.p="/my-addon/versions/main/assets/",(()=>{var e={143:0}';
     assert.equal(actual, expected);
   });
 });
