2f81d9b23a3adfc56abb14377e101ce9c41189a3
Robbie Wagner <rwwagner90@gmail.com>
2024-11-20 08:00:49 -0500

    Update test

--- tests-node/unit/deploy/plugin-test.js
+++ tests-node/unit/deploy/plugin-test.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const assert = require('chai').assert;
-const AddonDocsDeployPlugin = require('../../../lib/deploy/plugin');
+const {
+  replaceAddonDocsRootURL,
+} = require('../../../lib/utils/find-and-replace-in-directory');
 
 describe('`deploy` | plugin test', function () {
-  beforeEach(function () {
-    this.pluginInstance = new AddonDocsDeployPlugin();
-  });
-
-  it.skip('_macroReplaceIndexContent', function () {
+  it('replaceAddonDocsRootURL', function () {
     const contents = `
       <!DOCTYPE html>
       <html>
@@ -16,6 +14,8 @@ describe('`deploy` | plugin test', function () {
           <meta name="dummy/config/environment" content="%7B%22rootURL%22%3A%22%2FADDON_DOCS_ROOT_URL%2F%22%7D" />
         </head>
         <body>
+          <script src="ADDON_DOCS_ROOT_URL/assets/chunk.805.d0514e7e9edb751c6623.js"></script>
+          <script src="ADDON_DOCS_ROOT_URL/assets/chunk.524.385868f2db0b958b5ced.js"></script>
           <script src="/ADDON_DOCS_ROOT_URL/assets/vendor.js"></script>
           <script src="/ADDON_DOCS_ROOT_URL/assets/dummy.js"></script>
         </body>
@@ -31,7 +31,7 @@ describe('`deploy` | plugin test', function () {
       }),
     );
     const addonDocsRootURL = '/my-addon/versions/master/';
-    const actual = this.pluginInstance._macroReplaceIndexContent(
+    const actual = replaceAddonDocsRootURL(
       contents,
       addonDocsRootURL,
       encodedVersion,
@@ -43,6 +43,8 @@ describe('`deploy` | plugin test', function () {
           <meta name="dummy/config/environment" content="%7B%22rootURL%22%3A%22%2Fmy-addon%2Fversions%2Fmaster%2F%22%7D" />
         </head>
         <body>
+          <script src="/my-addon/versions/master/assets/chunk.805.d0514e7e9edb751c6623.js"></script>
+          <script src="/my-addon/versions/master/assets/chunk.524.385868f2db0b958b5ced.js"></script>
           <script src="/my-addon/versions/master/assets/vendor.js"></script>
           <script src="/my-addon/versions/master/assets/dummy.js"></script>
         </body>
